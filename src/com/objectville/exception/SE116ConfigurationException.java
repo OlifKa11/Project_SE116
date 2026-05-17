0650f660f16c25606027b30882cd40e76c22092e
Berkay <berkaykocofficial3545@gmail.com>
2026-05-17 17:44:33 +0300

    SE116ConfigurationException exception class added.

---
+++ src/com/objectville/exception/SE116ConfigurationException.java
@@ -0,0 +1,7 @@
+package com.objectville.exception;
+
+public class SE116ConfigurationException extends Exception{
+    public SE116ConfigurationException(String message){
+        super(message);
+    }
+}
