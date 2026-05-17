74f3a9b20db00347b563d325e6dad34657da8d34
Elif <elifmavigozm@gmail.com>
2026-05-17 16:01:23 +0300

    Updated distributor methods to accept map parameter

--- src/com/objectville/distribution/ServiceDistributor.java
+++ src/com/objectville/distribution/ServiceDistributor.java
@@ -1,11 +1,12 @@
 package com.objectville.distribution;
 
+import com.objectville.cells.Cell;
 import com.objectville.interfaces.Distributable;
 
 //Distributes services from service buildings to nearby zones.
 public class ServiceDistributor implements Distributable {
     @Override
-    public void distribute() {
+    public void distribute(Cell[][] map) {
 
     }
 }
--- src/com/objectville/distribution/UtilityDistributor.java
+++ src/com/objectville/distribution/UtilityDistributor.java
@@ -1,11 +1,12 @@
 package com.objectville.distribution;
 
+import com.objectville.cells.Cell;
 import com.objectville.interfaces.Distributable;
 
 //Distributes utilities from providers using breadth-first search.
 public class UtilityDistributor implements Distributable {
     @Override
-    public void distribute() {
+    public void distribute(Cell[][] map) {
 
     }
 }
