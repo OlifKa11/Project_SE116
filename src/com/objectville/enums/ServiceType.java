bc5978066245985395335b5c4c1655ab2181048f
Hasan Arda Aylar <2mhv2fqcyt@privaterelay.appleid.com>
2026-05-23 21:36:01 +0300

    start the project from scratch with new uml

---
+++ src/com/objectville/enums/ServiceType.java
@@ -0,0 +1,8 @@
+package com.objectville.enums;
+
+// Defins the types of services available in the city
+public enum ServiceType {
+    SECURITY,
+    HEALTH,
+    EDUCATION
+}
---
+++ src/com/objectville/enums/UtilityType.java
@@ -0,0 +1,8 @@
+package com.objectville.enums;
+
+// Defines basic utilties needed for zones to level up
+public enum UtilityType {
+    ELECTRICITY,
+    WATER,
+    INTERNET
+}
