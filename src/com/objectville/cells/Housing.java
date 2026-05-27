2f0210c39be240545ab42b9cc323940dc6a9ac14
yasmin161 <yasminyanik16@gmail.com>
2026-05-27 08:49:31 +0300

    Add lifestyle consumption and update methods in Housing

--- src/com/objectville/cells/Housing.java
+++ src/com/objectville/cells/Housing.java
@@ -1,4 +1,93 @@
 package com.objectville.cells;
 
 public class Housing {
+   private int consumedLifestyle;
+
+    public Housing(int x, int y) {
+        super(x, y, 'H');
+        this.consumedLifestyle = 0;
+    }
+    public void consumeLifestyle(int amount) {
+        this.consumedLifestyle = amount;
+    }
+
+    @Override
+    public void updateLevel() {
+
+        
+        if (electirictyReceived == 0 ||
+                waterReceived == 0 ||
+                internetReceived == 0) {
+
+            level = 0;
+            return;
+        }
+
+        
+        if (hasSecurity &&
+                hasHealth &&
+                hasEducation &&
+                consumedLifestyle > 0) {
+
+            if (level < 3) {
+                level++;
+            }
+        }
+
+        
+        else if (hasSecurity &&
+                hasHealth &&
+                hasEducation) {
+
+            if (level > 2) {
+                level--;
+            }
+
+            else if (level < 2) {
+                level++;
+            }
+        }
+
+        
+        else {
+
+            if (level > 1) {
+                level--;
+            }
+
+            else if (level < 1) {
+                level++;
+            }
+        }
+    }
+    @Override
+    public void calculateOutput() {
+
+        int m = Math.min(
+                electirictyReceived,
+                Math.min(waterReceived, internetReceived)
+        );
+
+        switch (level) {
+
+            case 0:
+                currentOutput = 0;
+                break;
+
+            case 1:
+                currentOutput = m;
+                break;
+
+            case 2:
+                currentOutput = 2 * m;
+                break;
+
+            default:
+                currentOutput = 2 * m + consumedLifestyle;
+                break;
+        }
+    }
+    public void resetTickData() {
+        consumedLifestyle = 0;
+    }
 }
