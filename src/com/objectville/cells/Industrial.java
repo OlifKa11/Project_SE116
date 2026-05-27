8fd7fcc88f55dfe0b1af5ea47441bd6602a5699e
yasmin161 <yasminyanik16@gmail.com>
2026-05-27 08:51:13 +0300

    Update Industrial.java

--- src/com/objectville/cells/Industrial.java
+++ src/com/objectville/cells/Industrial.java
@@ -1,4 +1,94 @@
 package com.objectville.cells;
 
 public class Industrial {
+  private int consumedPopulation;
+
+    public Industrial(int x, int y) {
+        super(x, y, 'I');
+        this.consumedPopulation = 0;
+    }
+    public void consumePopulation(int amount) {
+        this.consumedPopulation = amount;
+    }
+
+    @Override
+    public void updateLevel() {
+
+        
+        if (electirictyReceived == 0 ||
+                waterReceived == 0 ||
+                internetReceived == 0) {
+
+            level = 0;
+            return;
+        }
+
+        
+        if (hasSecurity &&
+                hasHealth &&
+                hasEducation &&
+                consumedPopulation > 0) {
+
+            if (level < 3) {
+                level++;
+            }
+        }
+
+        
+        else if (hasSecurity &&
+                hasHealth &&
+                hasEducation) {
+
+            if (level > 2) {
+                level--;
+            }
+
+            else if (level < 2) {
+                level++;
+            }
+        }
+
+        
+        else {
+
+            if (level > 1) {
+                level--;
+            }
+
+            else if (level < 1) {
+                level++;
+            }
+        }
+    }
+
+    @Override
+    public void calculateOutput() {
+
+        int m = Math.min(
+                electirictyReceived,
+                waterReceived
+        );
+
+        switch (level) {
+
+            case 0:
+                currentOutput = 0;
+                break;
+
+            case 1:
+                currentOutput = m;
+                break;
+
+            case 2:
+                currentOutput = 2 * m;
+                break;
+
+            default:
+                currentOutput = 2 * m + consumedPopulation;
+                break;
+        }
+    }
+    public void resetTickData() {
+        consumedPopulation = 0;
+    }
 }
