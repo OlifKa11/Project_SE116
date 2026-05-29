2e537b106e596e650dbe54ae294e85735b0bd0e3
Berkay <berkaykocofficial3545@gmail.com>
2026-05-29 19:14:56 +0300

    Completed getNeighbors() and getCellsWithinRadius() methods

--- src/com/objectville/engine/CityMap.java
+++ src/com/objectville/engine/CityMap.java
@@ -30,21 +30,53 @@ public class CityMap {
     // Used for Breadth-First Search (BFS) to find adjacent cells
     public List<Cell> getNeighbors(Cell cell) {
         List<Cell> neighbors = new ArrayList<>();
+        if (cell == null) {
+            return neighbors;
+        }
         int x = cell.getX();
         int y = cell.getY();
-
-
-
+        int[] dy={-1,1,0,0};
+        int[] dx={0,0,-1,1};
+        for (int i=0; i<4; i++){
+            int nextY = y + dy[i];
+            int nextX = x + dx[i];
+            if (nextX>=0 && nextX<width && nextY>=0 && nextY<height){
+                Cell neighbor = grid[nextX][nextY];
+                if (neighbor != null){
+                    neighbors.add(neighbor);
+                }
+            }
+        }
         return neighbors;
     }
 
     // Used for Services (Police, Hospital, School)
     public List<Cell> getCellsWithinRadius(Cell center, int radius) {
         List<Cell> inRadius = new ArrayList<>();
-
+        if (center == null || radius < 0) {
+            return inRadius;
+        }
+        int centerX = center.getX();
+        int centerY = center.getY();
+        int startY = Math.max(0, centerY - radius);
+        int endY = Math.min(height - 1, centerY + radius);
+        int startX = Math.max(0, centerX - radius);
+        int endX = Math.min(width - 1, centerX + radius);
+        for (int r = startY; r <= endY; r++) {
+            for (int c = startX; c <= endX; c++) {
+                int distance = Math.abs(centerX - c) + Math.abs(centerY - r);
+                if (distance <= radius) {
+                    Cell cell = grid[r][c];
+                    if (cell != null) {
+                        inRadius.add(cell);
+                    }
+                }
+            }
+        }
         return inRadius;
     }
-    public void printCity() {
+
+        public void printCity() {
         System.out.println("=== ObjectVille Current State ===");
         for (int r = 0; r < grid.length; r++) {
             for (int c = 0; c < grid[r].length; c++) {
